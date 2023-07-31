ff9fe1b1994a062e30b9f2222a605f0034814369
Chuong Quoc Nguyen <83415126+ChuongQuoc1413017@users.noreply.github.com>
2023-07-31 20:06:41 +0700

    Add files via upload

--- HyperRD/Halgorithm.py
+++ HyperRD/Halgorithm.py
@@ -6,6 +6,7 @@ Created on Mon Jul 17 19:50:30 2023
 """
 
 from HyperRD.Hgraph import *
+import itertools
 
 def vertex_connected(graph: object, start: object, end: object) -> bool:
     '''check whether two vertices are connected'''
@@ -76,3 +77,22 @@ def components_connected(graph: object) -> list:
         components.append(component)
         
     return components
+
+def graph_expansion(graph: object, mode: str) -> object:
+    '''expand hypergraph to normal graph'''
+    vertices = graph.vertices_dict
+    vertices_swap = {v: k for k, v in vertices.items()}
+    edges = graph.edges_dict
+    vertices_length = len(vertices)
+    edges_length = len(edges)
+    if mode == 'clique':
+        g = Create_Normal(vertices_length)
+        for edge in range(edges_length):
+            for i in itertools.combinations(edges[edge], 2):
+                g.add_edge(vertices_swap[i[0]], vertices_swap[i[1]])
+    elif mode == 'star':
+        g = Create_Normal(vertices_length + edges_length)
+        for edge in range(edges_length):
+            for i in edges[edge]:
+                g.add_edge(edge + vertices_length, vertices_swap[i])
+    return g
