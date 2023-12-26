b8a9c311775dce49807a9ec59217fea043691353
Chuong Quoc Nguyen <83415126+ChuongQuoc1413017@users.noreply.github.com>
2023-12-26 07:23:08 +0700

    Add files via upload

---
+++ HyperRD/Hmetric.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 17 19:50:30 2023
+
+@author: ASUS
+"""
+
+from HyperRD.Hgraph import *
+
+def density(graph):
+    '''return the density of hypergraph:
+       density = (the number of edges) / (the number of possible edges)'''
+    return len(graph.edges) / (2**len(graph.vertices) - 1)
+
+def girth(graph):
+    '''return the size of the smallest non-empty hyperedge in the hypergraph'''
+    return min(len(edge) for edge in graph.edges)
+
+def average_vertex_degree(graph):
+    graph.incidence_matrix()
+    '''return the average degree across all vertices in the hypergraph'''
+    total_degree = [graph.degree_vertex(graph.dict_of_vertices()[vertex]) for vertex in range(len(graph.vertices))]
+    return sum(total_degree) / len(total_degree)
+
+def average_edge_size(graph):
+    '''return the average size across all edges in the hypergraph'''
+    total_size = [len(edge) for edge in graph.edges]
+    return sum(total_size) / len(total_size)
